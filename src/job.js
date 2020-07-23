a3b710e2d872dde7c45a1cb5d2cd4e6c9fde72c9
Christer Johansson <christer.johansson@stenungsund.nu>
2020-07-23 22:50:15 +0200

    Added details in a collapsable text section. Added in some extra support for HTML

--- src/job.js
+++ src/job.js
@@ -1,22 +1,52 @@
-import React from "react";
-import { Card, Badge } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Badge, Button, Collapse } from "react-bootstrap";
+import Markdown from "react-markdown/with-html";
 
 export default function Job({ job }) {
+  const [open, setOpen] = useState(false);
   return (
     <Card>
       <Card.Body>
         <div className="d-flex justify-content-between">
           <div>
-  <Card.Title>{job.title} - <span className="text-muted font-weight-light">{job.company}</span>
+            <Card.Title>
+              {job.title} -{" "}
+              <span className="text-muted font-weight-light">
+                {job.company}
+              </span>
             </Card.Title>
             <Card.Subtitle className="text-muted mb-2">
-                {new Date(job.created_at).toLocaleDateString()}
+              {new Date(job.created_at).toLocaleDateString()}
             </Card.Subtitle>
-            <Badge variant="secondary" className="mr-2">{job.type}</Badge>
+            <Badge variant="secondary" className="mr-2">
+              {job.type}
+            </Badge>
             <Badge variant="secondary">{job.location}</Badge>
+            <div style={{ wordBreak: "break-all" }}>
+              <Markdown source={job.how_to_apply} escapeHtml={false} />
+            </div>
           </div>
+          <img
+            className="d-none d-md-block"
+            height="50"
+            alt={job.company}
+            src={job.company_logo}
+          />
         </div>
+        <Card.Text>
+          <Button
+            onClick={() => setOpen((prevOpen) => !prevOpen)}
+            variant="primary"
+          >
+            Show details
+          </Button>
+        </Card.Text>
+        <Collapse in={open}>
+          <div className="mt-4">
+            <Markdown source={job.description} escapeHtml={false} />
+          </div>
+        </Collapse>
       </Card.Body>
     </Card>
-  )
+  );
 }
