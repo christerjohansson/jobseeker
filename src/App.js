2d9e8b6f85046091ad56b818398dfccd2a3dba04
Christer Johansson <christer.johansson@stenungsund.nu>
2020-07-24 01:04:40 +0200

    Implemented a pagination feature, fetching 50 results at a time.

--- src/App.js
+++ src/App.js
@@ -8,20 +8,21 @@ import Spinner from './spinner';
 function App() {
   const [params, setParams] = useState({})
   const [page, setPage] = useState(1)
-  const {jobs, loading, error} = useFetchJobs(params, page)
+  const {jobs, loading, error, hasNextPage} = useFetchJobs(params, page)
 
   return (
     <Container className="my-4">
-      <h1 className="mb-4">Github JobSeeker</h1>
-      <JobsPagination page={page} setPage={setPage} />
-      {loading && <Spinner />}
+      <h1 className="text-center mb-4">Github JobSeeker</h1>
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
+      <div className="text-center">
+      {loading && <Spinner className="text-center"/>}
+      </div>
       {error && <h2>Something went wrong, try to reload.</h2>}
       {jobs.map(job => {
         return <Job key={job.id} job={job} />
       })}
-      <JobsPagination page={page} setPage={setPage} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
     </Container>
   )
 }
-
 export default App;
--- src/jobsPagination.js
+++ src/jobsPagination.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import {Pagination} from 'react-bootstrap'
+import React from "react";
+import { Pagination } from "react-bootstrap";
 
-export default function jobsPagination({page, setPage}) {
-    return (
-        <Pagination>
-            <Pagination.Prev />
-            <Pagination.Next />
-        </Pagination>
-    )
+export default function jobsPagination({ page, setPage, hasNextPage }) {
+    function adjustPage(amount){
+        setPage(prevPage => prevPage + amount)
+    }
+  return (
+    <Pagination>
+      {page !== 1 && <Pagination.Prev onClick={()=> adjustPage(-1)} />}
+      {page !== 1 && <Pagination.Item onClick={()=> setPage(1)}>1</Pagination.Item>}
+      {page > 2 && <Pagination.Ellipsis />}
+      {page > 2 && <Pagination.Item onClick={()=> adjustPage(-1)}>{page - 1}</Pagination.Item>}
+      <Pagination.Item active>{page}</Pagination.Item>
+      {hasNextPage && <Pagination.Item onClick={()=> adjustPage(1)}>{page + 1}</Pagination.Item>}
+      {hasNextPage && <Pagination.Next onClick={()=> adjustPage(1)} />}
+    </Pagination>
+  );
 }
