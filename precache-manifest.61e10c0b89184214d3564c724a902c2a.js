0d78414c180e272bdf3be860c789e901aef0b9ac
Christer Johansson <christer.johansson@stenungsund.nu>
2020-07-24 21:09:06 +0200

    Corrected the homepage URL in package.json

---
+++ precache-manifest.61e10c0b89184214d3564c724a902c2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffe3af40bf12df19faac2bb91b801e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8edc74600fcfac26b4ee",
+    "url": "./static/css/main.a51cf05f.chunk.css"
+  },
+  {
+    "revision": "15f168c4798058c21c60",
+    "url": "./static/js/2.8697bcd7.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "./static/js/2.8697bcd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8edc74600fcfac26b4ee",
+    "url": "./static/js/main.b38b3063.chunk.js"
+  },
+  {
+    "revision": "3100b7220c3fcfa26896",
+    "url": "./static/js/runtime-main.8c01a8a5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9b2f8d64aa14b30271f14d70be3fe15.js"
+  "./precache-manifest.61e10c0b89184214d3564c724a902c2a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
