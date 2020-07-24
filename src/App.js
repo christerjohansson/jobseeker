58aa9885634365461c6a42d03a7ffe9a72dde930
Christer Johansson <christer.johansson@stenungsund.nu>
2020-07-24 16:49:39 +0200

    Added logotype and fixed minor issues

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import useFetchJobs from "./useFetchJobs";
-import {Container} from 'react-bootstrap';
-import Job from './job';
-import JobsPagination from './jobsPagination';
-import Spinner from './spinner';
-import SearchForm from './SearchForm';
-
+import { Container, Image } from "react-bootstrap";
+import Job from "./job";
+import JobsPagination from "./jobsPagination";
+import Spinner from "./spinner";
+import SearchForm from "./SearchForm";
 
 function App() {
-  const [params, setParams] = useState({description: '', location: '', full_time: false});
-  const [page, setPage] = useState(1)
-  const {jobs, loading, error, hasNextPage} = useFetchJobs(params, page)
+  const [params, setParams] = useState({
+    description: "",
+    location: "",
+    full_time: false,
+  });
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
 
-  function handleParamChange(e){
-    const param = e.target.name
-    const value = e.target.value    
-    setPage(1)
-    setParams(prevParams => {
-      return {...prevParams, [param]: value}
-    })
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
   }
 
   return (
     <Container className="my-4">
-      <h1 className="text-center mb-4">Github JobSeeker</h1>
+      <Image
+        className="text-center logotype"
+        alt="Github Job Finder"
+        src="./logotype.png"
+      />
       <SearchForm params={params} onParamChange={handleParamChange} />
-      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       <div className="text-center">
-      {loading && <Spinner className="text-center"/>}
+        {loading && <Spinner className="text-center" />}
       </div>
       {error && <h2>Something went wrong, try to reload.</h2>}
-      {jobs.map(job => {
-        return <Job key={job.id} job={job} />
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
       })}
-      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage}/>
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </Container>
-  )
+  );
 }
 export default App;
