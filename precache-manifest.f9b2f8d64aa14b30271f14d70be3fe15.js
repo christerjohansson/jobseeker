fa3ba243b53c275208894bb06092ce872c27d269
Christer Johansson <christer.johansson@stenungsund.nu>
2020-07-24 18:49:37 +0200

    Open up build folder to subtree

---
+++ precache-manifest.f9b2f8d64aa14b30271f14d70be3fe15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673298a63e08e6b73473c084a41ba4b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8edc74600fcfac26b4ee",
+    "url": "/static/css/main.a51cf05f.chunk.css"
+  },
+  {
+    "revision": "15f168c4798058c21c60",
+    "url": "/static/js/2.8697bcd7.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.8697bcd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8edc74600fcfac26b4ee",
+    "url": "/static/js/main.b38b3063.chunk.js"
+  },
+  {
+    "revision": "4dac1f20d6143662e031",
+    "url": "/static/js/runtime-main.3534a7ef.js"
+  }
+]);
